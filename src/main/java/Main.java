d5e7264672186ff66b116b15eefa539b3e3b1d6c
wxh <wxhmessage@live.cn>
2019-03-18 16:37:32 +0800

    清理无用数据

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,5 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
